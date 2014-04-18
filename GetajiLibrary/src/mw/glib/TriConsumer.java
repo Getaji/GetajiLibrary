22d96bf49591e234d9eec43098790db56809666d
Getaji <t.tanaka.onion@gmail.com>
2014-04-18 19:06:11 +0900

    暫定的にBiConsumerとBiFunctionの3つ引数を取る版のクラスを追加。

---
+++ GetajiLibrary/src/mw/glib/TriConsumer.java
@@ -0,0 +1,42 @@
+package mw.glib;
+
+import java.util.Objects;
+
+/**
+ * 3つの引数を取り値を返さない関数インターフェースです。
+ *
+ * @author Getaji
+ */
+@FunctionalInterface
+public interface TriConsumer<T, U, V> {
+
+    /**
+     * Performs this operation on the given arguments.
+     *
+     * @param t the first input argument
+     * @param u the second input argument
+     * @param v the third input argument
+     */
+    void accept(T t, U u, V v);
+
+    /**
+     * Returns a composed {@code BiConsumer} that performs, in sequence, this
+     * operation followed by the {@code after} operation. If performing either
+     * operation throws an exception, it is relayed to the caller of the
+     * composed operation.  If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     *
+     * @param after the operation to perform after this operation
+     * @return a composed {@code BiConsumer} that performs in sequence this
+     * operation followed by the {@code after} operation
+     * @throws NullPointerException if {@code after} is null
+     */
+    default TriConsumer<T, U, V> andThen(TriConsumer<? super T, ? super U, ? super V> after) {
+        Objects.requireNonNull(after);
+
+        return (l, m, n) -> {
+            accept(l, m, n);
+            after.accept(l, m, n);
+        };
+    }
+}
