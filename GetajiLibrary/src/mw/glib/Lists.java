52e78010b324d12a420b00f440e79500f6f5acaa
Getaji <t.tanaka.onion@gmail.com>
2014-05-03 12:11:48 +0900

    コレクションの生成・操作をサポートするMapsとListsクラスを追加。

---
+++ GetajiLibrary/src/mw/glib/Lists.java
@@ -0,0 +1,133 @@
+package mw.glib;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+/**
+ * {@link java.util.List}の実装クラスを生成・操作するユーティリティクラスです。
+ *
+ * @author Getaji
+ */
+public class Lists {
+
+    /**
+     * 空の{@link ArrayList}を生成して返します。
+     *
+     * @param <E> 型
+     * @return 空のArrayList
+     */
+    public static <E> ArrayList<E> newArrayList() {
+        return new ArrayList<>();
+    }
+
+    /**
+     * 空の{@link LinkedList}を生成して返します。
+     *
+     * @param <E> 型
+     * @return 空のLinkedList
+     */
+    public static <E> LinkedList<E> newLinkedList() {
+        return new LinkedList<>();
+    }
+
+    /**
+     * 指定した数だけnullを格納した{@link ArrayList}を生成して返します。
+     *
+     * @param size 格納する数
+     * @param <E> 型
+     * @return 中身の入ったArrayList
+     */
+    public static <E> ArrayList<E> newArrayListWithItems(int size) {
+        ArrayList<E> list = newArrayList();
+        for (int i = 0; i < size; ++i)
+            list.add(null);
+        return list;
+    }
+
+    /**
+     * 指定した数だけ指定したアイテムを格納した{@link ArrayList}を生成して返します。
+     *
+     * @param size 格納する数
+     * @param item 格納するアイテム
+     * @param <E> 型
+     * @return 中身の入ったArrayList
+     */
+    public static <E> ArrayList<E> newArrayListWithItems(int size, E item) {
+        ArrayList<E> list = newArrayList();
+        for (int i = 0; i < size; ++i)
+            list.add(item);
+        return list;
+    }
+
+    /**
+     * 指定した配列に入ったアイテムを格納した{@link ArrayList}を生成して返します。
+     *
+     * @param items 格納するアイテムの配列
+     * @param <E> 型
+     * @return 中身の入ったArrayList
+     */
+    public static <E> ArrayList<E> newArrayListWithItems(E[] items) {
+        ArrayList<E> list = newArrayList();
+        Collections.addAll(list, items);
+        return list;
+    }
+
+    /**
+     * 指定した数だけnullを格納した{@link LinkedList}を生成して返します。
+     *
+     * @param size 格納する数
+     * @param <E> 型
+     * @return 中身の入ったLinkedList
+     */
+    public static <E> LinkedList<E> newLinkedListWithItems(int size) {
+        LinkedList<E> list = newLinkedList();
+        for (int i = 0; i < size; ++i)
+            list.add(null);
+        return list;
+    }
+
+    /**
+     * 指定した数だけ指定したアイテムを格納した{@link LinkedList}を生成して返します。
+     *
+     * @param size 格納する数
+     * @param item 格納するアイテム
+     * @param <E> 型
+     * @return 中身の入ったLinkedList
+     */
+    public static <E> LinkedList<E> newLinkedListWithItems(int size, E item) {
+        LinkedList<E> list = newLinkedList();
+        for (int i = 0; i < size; ++i)
+            list.add(item);
+        return list;
+    }
+
+    /**
+     * 指定した配列に入ったアイテムを格納した{@link LinkedList}を生成して返します。
+     *
+     * @param items 格納するアイテムの配列
+     * @param <E> 型
+     * @return 中身の入ったLinkedList
+     */
+    public static <E> LinkedList<E> newLinkedListWithItems(E[] items) {
+        LinkedList<E> list = newLinkedList();
+        Collections.addAll(list, items);
+        return list;
+    }
+
+    /**
+     * Listに入っているアイテムをインデックスと一緒にひとつずつ、受け取った関数に渡します。
+     *
+     * @param list 回すList
+     * @param action 処理する関数
+     * @param <E> 型
+     */
+    public static <E> void forEachWithIndex(List<E> list, BiConsumer<Integer, E> action) {
+        Checker.requireAndNonNull(list, action);
+        for (int i = 0; i < list.size(); ++i) {
+            action.accept(i, list.get(i));
+        }
+    }
+}
---
+++ GetajiLibrary/src/mw/glib/Maps.java
@@ -0,0 +1,110 @@
+package mw.glib;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+/**
+ * javadoc here.
+ *
+ * @author Getaji
+ */
+public class Maps {
+
+    /**
+     * 空の{@link HashMap}を生成して返します。
+     *
+     * @param <K> キーの型
+     * @param <V> 値の型
+     * @return 空のHashMap
+     */
+    public static <K, V> HashMap<K, V> newHashMap() {
+        return new HashMap<>();
+    }
+
+    /**
+     * 空の{@link LinkedHashMap}を生成して返します。
+     *
+     * @param <K> キーの型
+     * @param <V> 値の型
+     * @return 空のLinkedHashMap
+     */
+    public static <K, V> LinkedHashMap<K, V> newLinkedHashMap() {
+        return new LinkedHashMap<>();
+    }
+
+    /**
+     * 指定した配列に入ったアイテムを格納した{@link HashMap}を生成して返します。
+     *
+     * @param keys 格納するアイテムのキーの配列
+     * @param values 格納するアイテムの値の配列
+     * @param <K> キーの型
+     * @param <V> キーの型
+     * @return 中身の入ったHashMap
+     */
+    public static <K, V> HashMap<K, V> newHashMapWithItems(K[] keys, V[] values) {
+        Checker.requireAndNonNull(keys, values);
+        HashMap<K, V> map = newHashMap();
+        for (int i = 0; i < keys.length; ++i) {
+            map.put(keys[i], values[i]);
+        }
+        return map;
+    }
+
+    /**
+     * 指定した配列に入ったアイテムを格納した{@link LinkedHashMap}を生成して返します。
+     *
+     * @param keys 格納するアイテムのキーの配列
+     * @param values 格納するアイテムの値の配列
+     * @param <K> キーの型
+     * @param <V> 値の型
+     * @return 中身の入ったLinkedHashMap
+     */
+    public static <K, V> LinkedHashMap<K, V> newLinkedHashMapWithItems(K[] keys, V[] values) {
+        Checker.requireAndNonNull(keys, values);
+        LinkedHashMap<K, V> map = newLinkedHashMap();
+        for (int i = 0; i < keys.length; ++i) {
+            map.put(keys[i], values[i]);
+        }
+        return map;
+    }
+
+    /**
+     * Mapに入っているアイテムをインデックスと一緒にひとつずつ、受け取った関数に渡します。
+     * キーと値は別々に渡されます。
+     *
+     * @param map 回すMap
+     * @param action 処理する関数
+     * @param <K> キーの型
+     * @param <V> 値の型
+     */
+    public static <K, V> void forEachWithIndex(Map<K, V> map, TriConsumer<Integer, K, V> action) {
+        Checker.requireAndNonNull(map, action);
+        Iterator<Map.Entry<K, V>> iterator = map.entrySet().iterator();
+        Map.Entry<K, V> entry;
+        for (int i = 0; i < map.size(); ++i) {
+            entry = iterator.next();
+            action.accept(i, entry.getKey(), entry.getValue());
+        }
+    }
+
+    /**
+     * Mapに入っているアイテムをインデックスと一緒にひとつずつ、受け取った関数に渡します。
+     * キーと値は{@link Map.Entry}で渡されます。
+     *
+     * @param map 回すMap
+     * @param action 処理する関数
+     * @param <K> キーの型
+     * @param <V> 値の型
+     */
+    public static <K, V> void forEachWithIndex(Map<K, V> map,
+                                               BiConsumer<Integer, Map.Entry<K, V>> action) {
+        Checker.requireAndNonNull(map, action);
+        Iterator<Map.Entry<K, V>> iterator = map.entrySet().iterator();
+        for (int i = 0; i < map.size(); ++i) {
+            action.accept(i, iterator.next());
+        }
+    }
+}
